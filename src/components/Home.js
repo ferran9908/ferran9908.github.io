65b90ac3d720a5880916961c258148b0b47e72c6
Ferran Sulaiman <ferran.9908@gmail.com>
2020-12-13 02:31:12 +0530

    Finish HomePage; Finish Blog Page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,19 @@
+import image from "../cover.jpg";
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <main>
+      <img
+        src={image}
+        alt="Macbook"
+        className="absolute object-cover h-full w-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64">
+        <h1 className="text-6xl text-white font-bold my-style leading-none lg:leading-snug home-name">
+          Hey, I'm Ferran
+          <br />
+          I'm a fullstack developer
+        </h1>
+      </section>
+    </main>
+  );
 }
