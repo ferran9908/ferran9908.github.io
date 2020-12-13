8e2a9d5ae250ad8f013a19993d3126dc4b9333f6
Ferran Sulaiman <ferran.9908@gmail.com>
2020-12-13 12:16:29 +0530

    Ad hover to post object

--- src/components/Post.js
+++ src/components/Post.js
@@ -44,7 +44,7 @@ export default function Post() {
                       className="w-full h-full rounded-r object-cover absolute"
                     />
                     <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
-                      <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-red-800 text-red-100 bg-opacity-75 rounded">
+                      <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-red-800 hover:bg-red-900 text-red-100 bg-opacity-75 rounded">
                         {post.title}
                       </h3>
                     </span>
